84718d7d07db8182a480d499edef3a79e88dcdeb
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2026-01-19 20:40:47 +0100

    Set larger K parameter

--- Main.m
+++ Main.m
@@ -2,7 +2,7 @@ pkg load parallel
 
 alphas = [0.99, 0.95, 0.9, 0.85, 0.8, 0.75]; % honest stake ratio
 deltas = [2, 3, 4, 5]; % network delay in slots
-KK = 3;
+KK = 25;
 
 params = zeros([(length(alphas) * length(deltas))  3]);
 index = 1;
